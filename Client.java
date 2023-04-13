61dedb4963bc94f62bb08ba4665dd3c64917d6a1
Dom G <domgarguilo@apache.org>
2023-04-13 14:31:23 -0400

    Add Client.ServerConnection Object

--- Client.java
+++ Client.java
@@ -8,10 +8,61 @@ import java.util.Scanner;
 
 class Client {
 
+    /**
+     * Object to encapsulate the communication to and from the server.
+     */
+    private static class ServerConnection implements AutoCloseable {
+
+        private final Socket clientSocket;
+        private final DataOutputStream outToServer;
+        private final InputStreamReader inputStreamReader;
+        private final BufferedReader inFromServer;
+
+        public ServerConnection(String host, int port) throws IOException {
+            this.clientSocket = new Socket(host, port);
+            this.outToServer = new DataOutputStream(clientSocket.getOutputStream());
+            this.inputStreamReader = new InputStreamReader(clientSocket.getInputStream());
+            this.inFromServer = new BufferedReader(inputStreamReader);
+        }
+
+        /**
+         * Sends a String to the server
+         *
+         * @param message String to send to the server
+         */
+        private void sendMsgToServer(String message) throws IOException {
+            this.outToServer.writeBytes(message);
+            this.outToServer.flush();
+        }
+
+        /**
+         * Reads a line from the given BufferedReader
+         *
+         * @return A String of the line read from the reader
+         */
+        private String receiveMsgFromServer() throws IOException {
+            return this.inFromServer.readLine();
+        }
+
+        @Override
+        public void close() throws Exception {
+            if (clientSocket != null) {
+                clientSocket.close();
+            }
+            if (inFromServer != null) {
+                inFromServer.close();
+            }
+            if (inputStreamReader != null) {
+                inputStreamReader.close();
+            }
+            if (outToServer != null) {
+                outToServer.close();
+            }
+        }
+    }
+
     public static final String GAME_OVER = "Game over.";
     public static final String PRESS_ENTER = "press enter";
-    private static String host = "localhost";
-    private static int port = 1337;
 
     private static final String WELCOME_MSG = "--- Welcome to the card game 'war'\n";
     private static final String RULES_MSG = "\nRule set:\n - Each player flips one card at a time\n - Whoever had the highest card value takes both cards \n - "
@@ -25,29 +76,25 @@ class Client {
         System.out.print(Client.WELCOME_MSG);
         System.out.print(Client.RULES_MSG);
 
-        boolean autoFlip;
-
         try (Scanner clientInputScanner = new Scanner(System.in)) {
-            host = Client.getHost(clientInputScanner);
-            port = Utils.getPort(clientInputScanner, port);
-            autoFlip = getAutoFlip(clientInputScanner);
+
+            final String host = Client.getHost(clientInputScanner, "localhost");
+            final int port = Utils.getPort(clientInputScanner, 1337);
+            final boolean autoFlip = getAutoFlip(clientInputScanner);
 
             // initializing the client socket, output to server and input from server
-            try (Socket clientSocket = new Socket("localhost", Client.port);
-                 DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-                 InputStreamReader inputStreamReader = new InputStreamReader(clientSocket.getInputStream());
-                 BufferedReader inFromServer = new BufferedReader(inputStreamReader)) {
+            try (ServerConnection serverConnection = new ServerConnection(host, port)) {
 
-                sendMsgToServer("Client to server test" + "\n", outToServer); // output test
+                serverConnection.sendMsgToServer("Client to server test\n"); // output test
                 System.out.print("\nClient to server test msg sent\n");
-                response = receive(inFromServer); // input test
+                response = serverConnection.receiveMsgFromServer(); // input test
                 System.out.print("Test msg from server received: " + response + "\n");
 
                 System.out.print("Ready to begin. Press enter to start game");
                 var ignore = System.in.read(); // wait for user to press enter
 
                 // Constantly listening for messages from the server
-                while ((response = receive(inFromServer)) != null) {
+                while ((response = serverConnection.receiveMsgFromServer()) != null) {
 
                     // Print response
                     System.out.print("\nServer: " + response);
@@ -62,7 +109,7 @@ class Client {
                             var ignore1 = System.in.read(); // wait for user to press enter
                         }
                         // Tell server to flip next card
-                        sendMsgToServer(input, outToServer);
+                        serverConnection.sendMsgToServer(input);
                         System.out.print("\nYour card was successfully sent to the server."
                                 + " Waiting on result from the server...");
                     }
@@ -74,37 +121,22 @@ class Client {
     }
 
     /**
-     * Sends a String to the server
+     * Ask the user what host they want to connect to.
      *
-     * @param message      String to send to the server
-     * @param outputStream DataOutputStream to use to send the message
+     * @param inputScanner Scanner object to use to get user input
+     * @param defaultHost  host to default to if user chooses to
+     * @return the host that the user has chosen
      */
-    private static void sendMsgToServer(String message, DataOutputStream outputStream) throws IOException {
-        outputStream.writeBytes(message);
-        outputStream.flush();
-    }
-
-    /**
-     * Reads a line from the given BufferedReader
-     *
-     * @param bufferedReader the BufferedReader from which to read
-     * @return A String of the line read from the reader
-     */
-    private static String receive(BufferedReader bufferedReader) throws IOException {
-        return bufferedReader.readLine();
-    }
-
-    // gets port from user
-    public static String getHost(Scanner inputScanner) {
+    public static String getHost(Scanner inputScanner, String defaultHost) {
         System.out.print("Please enter the servers ip address or\n");
         System.out.print("insert \"0\" to connect to server running on this machine (localhost): ");
         String input = inputScanner.next();
 
-        return Objects.equals(input, "0") ? Client.host : input;
+        return Objects.equals(input, "0") ? defaultHost : input;
     }
 
     /**
-     * Prompt the user if they want to enable automatic flipping of cards
+     * Ask the user if they want to enable automatic flipping of cards
      *
      * @param inputScanner the scanner to read user input from
      * @return true if auto flip is to be enabled, else false
